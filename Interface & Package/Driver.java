89c7fcabb44f1b8b2cd30789420459b3403e4b65
Jaskirat Singh Rangi <143155832+gigasxx@users.noreply.github.com>
2026-06-06 10:11:16 +0530

    Driver class to manage Student data

---
+++ Interface & Package/Driver.java
@@ -0,0 +1,10 @@
+import Pack1.Student;
+public class Driver
+{
+    public static void main(String[] args)
+    {
+        Student s = new Student();
+        s.getStudentData();
+        s.printStudentData();
+    }
+}
